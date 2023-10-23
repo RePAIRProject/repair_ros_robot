d42562624e0641b3f71ba990c0d2f5c2819ce7fd
Benedikt Kreis <kreis@cs.uni-bonn.de>
2023-10-23 18:07:44 +0200

    Changed repair_interface so that it can deal with left and right hand as well as with the real robot and gazebo. moveit_test.py has to be adapted

--- repair_interface/scripts/qbhand_test.py
+++ repair_interface/scripts/qbhand_test.py
@@ -3,6 +3,7 @@
 # this version works on python3 ubuntu 20
 import rospy
 
+from std_msgs.msg import Float64
 from std_msgs.msg import Float32
 from ec_msgs.msg import HandCmd
 
@@ -10,59 +11,79 @@ FREQ = 200
 
 
 class QbHand:
-    def __init__(self):
-        self.gripperMsg = HandCmd()
+    def __init__(self, side= "right", gazebo=False):
+        self.side = side
+        self.gazebo = gazebo
+        if gazebo:
+            self.gripperMsg = Float64()
+            self.open_value = 0.0
+            self.close_value = 1.0
+        else:
+            self.gripperMsg = HandCmd()
+            self.open_value = 0.0
+            self.close_value = 19000.0
         self.init_ros()
         self.init_params()
-
+        
         rospy.sleep(1)
 
-    def close_hand(self, aperture, secs=1):
-        # close
-        print('Closing qb Soft Hand..')
-        pose = aperture
-        self.gripperMsg.pos_ref = pose
+    def move_hand(self, aperture, secs=1):
+        # moving
+        #print('Moving qb Soft Hand..')
+        if self.gazebo:
+            self.gripperMsg.data = aperture
+        else:
+            self.gripperMsg.pos_ref = aperture
         # print(self.gripperMsg)
         self.GripperPub.publish(self.gripperMsg)
 
         print('wait to finish')
         rospy.sleep(secs)
+
+    def close_hand(self):
+        # close
+        print('Closing qb Soft Hand..')
+        self.move_hand(self.close_value)
+
 
     def open_hand(self, secs=1):
         # open
-        print('Opening..')
-        pose = 0
-        self.gripperMsg.pos_ref = pose
-        # print(self.gripperMsg)
-        self.GripperPub.publish(self.gripperMsg)
-
-        print('wait to finish')
-        rospy.sleep(secs)
+        print("Opening qb Soft Hand..")
+        self.move_hand(self.open_value)
 
     def init_params(self):
         pass
 
     def init_ros(self):
         try:
-            rospy.init_node('Qb_hand', anonymous=True)
+            rospy.init_node("Qb_hand_"+self.side, anonymous=True)
         except rospy.exceptions.ROSException as e:
             var = 0  # print("Node has already been initialized, do nothing")
 
         self.rate = rospy.Rate(FREQ)
         # Simulation topic
-        # hand_topic = "/left_hand_v1s/synergy_command"
+        # hand_topic = "/"+side+"_hand_v1s/synergy_command"
 
         #rostopic pub /xbotcore/left_hand/command ec_msgs/HandCmd "{pos_ref: 0.0, pos_ref_2: 0.0, pos_ref_3: 0.0, vel_ref: 0.0, tor_ref: 0.0}"
         # 19000 close
 
-        # real tobot topic
-        hand_topic = "/xbotcore/right_hand/command"
-        self.GripperPub = rospy.Publisher(hand_topic,
-                                          HandCmd, queue_size=3)
+        if(self.gazebo):
+            self.GripperPub = rospy.Publisher("/"+self.side+"_hand_v1s/synergy_command", Float64, queue_size=3)
+
+        else:
+            hand_topic = "/xbotcore/"+self.side+"_hand/command"
+            self.GripperPub = rospy.Publisher(hand_topic, HandCmd, queue_size=3)
         
 if __name__ == "__main__":
-    hand = QbHand()
-    value = 19000
-    hand.close_hand(value)
-    hand.open_hand()
+    gazebo = True
+    side = "right"
+    hand_api = QbHand(side, gazebo)
+
+    if hand_api.gazebo:
+        value = 0.5
+    else:
+        value = 19000.0/2
+    hand_api.move_hand(value)
+    hand_api.close_hand()
+    hand_api.open_hand()
     print('Finish!')
